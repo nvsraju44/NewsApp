e7c43c2630db64af752d483512e92e2f24a5e1e8
nvsraju <nvsraju44@gmail.com>
2021-02-04 01:33:02 +0530

    removed unwanted code

--- NewsApp/Model/NewsList.swift
+++ NewsApp/Model/NewsList.swift
@@ -7,16 +7,3 @@ struct Article: Codable {
 struct NewsList: Codable {
     let articles: [Article]
 }
-
-extension NewsList: Equatable {
-    static func == (lhs: NewsList, rhs: NewsList) -> Bool {
-      return lhs.articles == rhs.articles
-    }
-}
-
-extension Article: Equatable {
-    static func == (lhs: Article, rhs: Article) -> Bool {
-        return lhs.title == rhs.title && lhs.description == rhs.description
-    }
-}
-
